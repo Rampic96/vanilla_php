386b7e739375616c89e9aef569f940637ebfdf21
Moletastic <boxjack.romero@gmail.com>
2018-10-12 18:04:31 -0300

    class Cancion

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 <?php
+
+class Cancion{
+    
+    private $id;
+    private $nombre;
+    private $genero;
+    private $artista;
+    
+    public function __construct($id, $nombre, $genero, $artista ){
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->genero = $genero;
+        $this->artista = $artista;
+    }
+
+    public function toString() : string{
+        return "{$this->nombre} [{$this->artista}]" ; 
+    }
+
+}
+
+$cancion = new Cancion(1,"Last Train to Home", "Jazz", "Pat Metheny");
+
+echo $cancion->toString();
+
